d2668a2a81616624130b0700b85b5247a41e5bfa
Vitali Griunvald <vgriunvald@gmail.com>
2021-05-03 22:47:15 +0300

    Add styles to the buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,25 +8,25 @@ const App = () => {
       <div className="container">
         <div className="display">0</div>
         <div className="body">
-          <Button value="AC" />
-          <Button value="±" />
-          <Button value="%" />
-          <Button value="÷" />
-          <Button value="7" />
-          <Button value="8" />
-          <Button value="9" />
-          <Button value="×" />
-          <Button value="4" />
-          <Button value="5" />
-          <Button value="6" />
-          <Button value="—" />
-          <Button value="1" />
-          <Button value="2" />
-          <Button value="3" />
-          <Button value="+" />
-          <Button value="0" />
-          <Button value="." />
-          <Button value="=" />
+          <Button value="AC" className="button function" />
+          <Button value="±" className="button function" />
+          <Button value="%" className="button function" />
+          <Button value="÷" className="button operator" />
+          <Button value="7" className="button" />
+          <Button value="8" className="button" />
+          <Button value="9" className="button" />
+          <Button value="×" className="button operator" />
+          <Button value="4" className="button" />
+          <Button value="5" className="button" />
+          <Button value="6" className="button" />
+          <Button value="—" className="button operator" />
+          <Button value="1" className="button" />
+          <Button value="2" className="button" />
+          <Button value="3" className="button" />
+          <Button value="+" className="button operator" />
+          <Button value="0" className="button zero" />
+          <Button value="." className="button" />
+          <Button value="=" className="button operator" />
         </div>
       </div>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ import './Button.css';
 
 interface Props {
   value: string;
+  className: string;
   // onClick: () => string;
 }
 
-const Button: React.FC<Props> = ({ value }) => {
-  return <div className={`button ${value === '0' ? 'zero' : ''}`}>{value}</div>;
+const Button: React.FC<Props> = ({ value, className }) => {
+  return <div className={className}>{value}</div>;
 };
 
 export default Button;
